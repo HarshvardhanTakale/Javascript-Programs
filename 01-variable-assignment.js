b97f8062ed8a4267d92f295570d97cf88eac2744
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2024-07-31 08:08:06 +0530

    variable assignment
    variable assignment

---
+++ 01-variable-assignment.js
@@ -0,0 +1,5 @@
+console.log("My City Name is: Sangola");
+console.log("My Friend Name is: Hanmant");
+console.log("My Country Name is:  India");
+console.log("My School Name is: Anand Vidyalaya Kamlapur ");
+console.log("My Collage Name is: Dr.Ganpatrao Deshmuksh Mahavidhyalaya Sangola");
---
+++ 01-variable.js
@@ -0,0 +1,15 @@
+//  variable declaration 
+var firstName;
+console.log(firstName);
+
+//  variable initialization
+firstName="Harsh";
+console.log(firstName);
+
+// variable Declaration and initialization
+var lastName="Takale";
+console.log(lastName);
+
+// variable update
+firstName="Harshvardhan";
+console.log(firstName);
