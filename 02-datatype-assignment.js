7f8b4787979dbf6bd539c9bde69629641e3af799
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2024-07-31 15:55:01 +0530

    javascript datatype assignment2
    javascript datatype assignment2

---
+++ 02-datatype-assignment.js
@@ -0,0 +1,37 @@
+console.log("============================ 1 ========================");
+var bankName
+console.log("Bank Name is:", bankName);
+var bankName="SBI";
+console.log("Bank Name is:",bankName);
+var bankNameTypeOf = typeof bankName;
+console.log("Bank Name Datatype is: ", bankNameTypeOf);
+
+console.log("========================== 2  =========================");
+var marks
+console.log("My Mark is:",marks);
+var marks = "80%";
+console.log("My Mark is: ", marks);
+var marksTypeOf = typeof marks;
+console.log("Datatype of marks is:", marksTypeOf);
+console.log("=========================== 3  ========================");
+var status 
+console.log("My Working Status is:",status);
+var isstatus=true;
+console.log("My Working Status is:",isstatus);
+var isstatusTypeof = typeof isstatus;
+console.log("My Working Status Datatype is:",isstatusTypeof);
+console.log("=========================== 4  ========================");
+console.log("************************** 1****************************8");
+var count
+console.log("Total Count is:",count);
+var count="Hundred and Seven ";
+console.log("Total Count is:",count);
+var countTypeof = typeof count;
+console.log("Total Count Datatype is:",countTypeof);
+console.log("************************** 1****************************8");
+var count = 107;
+console.log("Update Total Count is:",count);
+var countTypeof = typeof count;
+console.log("Update Total Count Datatype is:",countTypeof);
+
+
