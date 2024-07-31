d9210a0f6e4b91e8c594c86c983e424709c9ef41
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2024-07-31 14:45:21 +0530

    datatype assignment1
    datatype assignment1

---
+++ 01-datattype-assignment.js
@@ -0,0 +1,34 @@
+console.log("================ 1  ==============");
+var myFullName 
+console.log("My Full Name Value Before initialization ::",myFullName );
+var FullName="Harshvardhan Takale ";
+console.log("My First Name and Last Name After initialization is:",FullName);
+var FullName="Harshvardhan  Madhukar Takale ";
+console.log("After Updating My Full Name Is :",FullName);
+
+console.log("============== 2 ==================");
+var MyLove="I Love only Js";
+console.log(MyLove);
+
+
+console.log("============== 3 ==================");
+var FirstName="Harsh";
+console.log("My First Name is :",FirstName);
+var LastName="Takale";
+console.log("My Last Name is :",LastName);
+var Age=23;
+console.log("My Age Name is :",Age);
+var Address="sangola";
+console.log("My Address is :",Address);
+var pin=413307;
+console.log("My Pincode is :",pin);
+var Gender="Male";
+console.log("My Gender is :",Gender);
+var isMarried = false;
+console.log(" My Married Status is : ", isMarried);
+var isMarriedTypeOf = typeof isMarried;
+
+
+
+
+
