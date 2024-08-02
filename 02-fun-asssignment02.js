5c37946c277384c860646c9a7dd5d0edb5c8eff6
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2024-08-02 14:34:49 +0530

    function assignmnt 2
    function assignmnt 2

--- 02-fun-asssignment02.js
+++ 02-fun-asssignment02.js
@@ -3,7 +3,7 @@ var bankName="CITI  ||"; var accountNum="3456782345  || "; var location="Pune  |
 console.log("Bank Nme is :",bankName,"Account Number is :",accountNum,"Location is :",location,"PinCode is :",pinCode);
 var bankName="Axis  ||"; var accountNum="7856782345  || "; var location="Mumbai  || ";var pinCode=441202;
 console.log("Bank Nme is :",bankName,"Account Number is :",accountNum,"Location is :",location,"PinCode is :",pinCode);
-var bankName="HDFC  ||"; var accountNum="8956782345  || "; var location="Pune  || ";var pinCode=631202; 
+var bankName="HDFC ||"; var accountNum="8956782345  || "; var location="Pune  || ";var pinCode=631202; 
 console.log("Bank Nme is :",bankName,"Account Number is :",accountNum,"Location is :",location,"PinCode is :",pinCode,);
 }
 bankDetails();
