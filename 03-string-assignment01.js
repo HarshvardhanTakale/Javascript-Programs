f3099b847d5b37e11563d47dbcd545ec8011a70f
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2024-08-04 11:45:28 +0530

    string  assignment 1
    string assignment 1

---
+++ 03-string-assignment01.js
@@ -0,0 +1,12 @@
+var dream = "TCS";
+console.log(`My Dream Company is :${"TCS"}`);
+var hobby1="Reading";
+var hobby2="Coding";
+var hobby3="Problem Solving";
+console.log(`My Hobbies Are: ${"Reading"},${"Coding"},${"Problem Solving"}`);
+
+var totallength= hobby1.length+hobby2.length+hobby3.length;
+console.log("Total Sum is:",totallength);
+
+
+
