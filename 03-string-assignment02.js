66d5695eb0f0f03decca26e1c918de2bb38af099
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2024-08-04 14:25:18 +0530

    string assignment 2
    string assignment 2

---
+++ 03-string-assignment02.js
@@ -0,0 +1,35 @@
+ function stringHandsOn() {
+console.log("================================ Step1 =====================================");
+var string = "    Hey you are doing good,keep it up    ";
+console.log("The Given String is:",string);
+console.log("================================ Step2 =====================================");
+ var result = string.length;
+ console.log("The Length of String is:",result);
+ console.log("================================ Step3 =====================================");
+ var string = "  sHey you are doing good,keep it up    ";
+ var string1 = string.trim();
+ console.log("After Trim String: ",string);
+ var AfterLength = string1.length;
+ console.log("Length is : ",AfterLength);
+ console.log("================================ Step4 =====================================");
+ console.log("Total number of extra space removed",result-AfterLength);
+ console.log("================================ Step5 =====================================");
+ var firstString = string1.charAt(0);
+ var lastString = string1.charAt(26);
+ console.log("The first & last character are =",firstString,lastString);
+ console.log("================================ Step6 =====================================");
+ var string =string.split(" ");
+ console.log("Total words available after step 3 = ",string.length);
+ console.log("================================ Step7 =====================================");
+var index = string1.indexOf("good");
+console.log("index of good",index);
+ console.log("================================ Step8 =====================================");
+ console.log("substring starting from index 22 = ",string1.substring(22));
+ console.log("================================ Step9=====================================");
+ var result = string.includes("up");
+console.log("The string ends with up = ",result);
+ console.log("================================ Step10 =====================================");
+ var result = string.includes("Hey");
+ console.log("The string start with up = ",result);
+ }
+ stringHandsOn();
