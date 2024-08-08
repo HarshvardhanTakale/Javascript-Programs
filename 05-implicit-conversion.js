1e620b8455398b4a8e168d732933fda2bf1b680e
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2024-08-08 15:47:40 +0530

    implicit conversion assignment
    implicit conversion assignment

---
+++ 05-implicit-conversion.js
@@ -0,0 +1,17 @@
+var result='3'+2;
+console.log(result);
+
+console.log("====================");
+
+var result='3'+true;
+console.log(result);
+
+console.log("====================");
+
+var result='3'+undefined;
+console.log(result);
+
+console.log("====================");
+
+var result='3'+null;
+console.log(result);
