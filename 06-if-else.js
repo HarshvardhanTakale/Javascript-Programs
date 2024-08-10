a7ece4ed8b88bf53dd5e466da6439ab44c8a4040
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2024-08-10 13:30:34 +0530

    for loop assignmnt 1
    for loop assignmnt 1

---
+++ 06-if-else.js
@@ -0,0 +1,9 @@
+var votingChecker = function(age, person){
+    if (age>=18) {
+        console.log(`Hey ${person} your age is ${age} and you are eligible for vote`);
+    } else {
+        console.log(`Unfortunately , Hey ${person} your age is ${age} and you are not eligible for vote`);
+    }
+}
+votingChecker(21, "Harsh");
+votingChecker(17, "Hanmant");
